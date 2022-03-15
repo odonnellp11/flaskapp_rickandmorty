fed0bbff312bf9fc29f45e4916db7a7db14a0b96
odonnellp11 <96839471+odonnellp11@users.noreply.github.com>
2022-03-14 22:26:38 -0400

    basiclayout

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path= '/static', static_folder= 'static')
 app.config.from_object(Config)
 
 from . import routes
--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,14 @@ from flask import render_template
 def home():
     return render_template('index.html')
 
-@app.route('/charaters')
+@app.route('/characters')
 def characters():
     return render_template('char.html')
+
+@app.route('/rick')
+def Ricks():
+    return render_template('/rick.html')
+
+@app.route('/morty')
+def mortys():
+    return render_template('/morty.html')
