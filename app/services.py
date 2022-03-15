e2fcaed5a5be0450d7a0fedee12d78cad3bd7f15
odonnellp11 <96839471+odonnellp11@users.noreply.github.com>
2022-03-15 02:10:17 -0400

    update

---
+++ app/services.py
@@ -0,0 +1,16 @@
+import requests as r
+
+
+
+def char_image():
+    response = r.get('https://rickandmortyapi.com/api/character/1')
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        return response.status_code
+   
+    chars = {}
+    for name in data:
+        if data[name]:
+            chars[name] = data[name]
+    return chars
